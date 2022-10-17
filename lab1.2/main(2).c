9ef724dfb2f48f5fd664f0d0aa9f3b8aad9c2996
KaterynaFol <114384676+KaterynaFol@users.noreply.github.com>
2022-10-17 14:07:43 +0300

    Add files via upload

---
+++ lab1.2/main(2).c
@@ -0,0 +1,113 @@
+// LABKU ROBILI: SYZKO, SYLA, FOLVARKOVA //
+// LABKU ROBILI: SYZKO, SYLA, FOLVARKOVA //
+// LABKU ROBILI: SYZKO, SYLA, FOLVARKOVA //
+// LABKU ROBILI: SYZKO, SYLA, FOLVARKOVA //
+// LABKU ROBILI: SYZKO, SYLA, FOLVARKOVA //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+int main()
+{
+	double F, x1, x2, delta;
+	uint32_t var, N;
+
+	printf("Variant vedennya 1 or 2? : "); // vibor metoda
+	scanf("%u", &var);
+
+	//validacia//
+	while (var != 1 && var != 2)
+	{
+		printf("ERROR! Reenter ihfo\n\r");
+		printf("Variant vedennya 1 or 2? : ");
+		scanf("%u", &var);
+	}
+	//validacia//
+
+	if (var == 1)
+	{
+
+		//vvod dannih//
+		printf("\n\rx1 = ");
+		scanf("%lf", &x1);
+		printf("\n\rx2 = ");
+		scanf("%lf", &x2);
+		printf("\n\r(N>0) N = ");
+		scanf("%u", &N);
+		//vvod dannih//
+
+		//validacia//
+		while (N < 1)
+		{
+			printf("ERROR! Vvedit N > 0 \n\r");
+			printf("N=");
+			scanf("%u", &N);
+		}
+		//validacia//
+
+		int i = 1;
+		delta = (x2 - x1) / (N - 1);
+		system("cls");
+		printf("\n\r----------------------------------");
+		printf("\n\r|   N   |     X     |      F(X)     |");
+		printf("\n\r----------------------------------\n\r");
+		for (; i <= N; i++)
+		{
+			printf("|%4d   |%10.2f  |%12.2f |", i, x1, F);
+			printf("\n\r----------------------------------\n\r");
+			x1 += delta;
+			F = (pow(x1, 3) / -25 + 10 * pow(x1, 2) - 15000);
+			////////////////////////////zakonchilos` mesto////////////////////////////////
+			if (i % 12 == 0)
+			{
+				printf("Press any key\n\r");
+				getch();
+			}
+			////////////////////////////zakonchilos` mesto////////////////////////////////
+
+		}
+	}
+
+	else
+	{
+
+		printf("\nx1 = ");
+		scanf("%lf", &x1);
+		printf("\nx2 = ");
+		scanf("%lf", &x2);
+		printf("\ndelta = ");
+		scanf("%lf", &delta);
+
+		//validacia//
+		while (delta < 1.0)
+		{
+			printf("ERROR! Vvedit delta > 0 \n");
+			printf("delta=");
+			scanf("%lf", &delta);
+		}
+		//validacia//
+		int i = 1;
+		system("cls");
+		printf("\n\r----------------------------------");
+		printf("\n\r|   N   |     X     |      F(X)     |");
+		printf("\n\r----------------------------------\n\r");
+		while (x1 <= x2)
+		{
+			printf("|%4d   |%10.2f  |%12.2f |", i, x1, F);
+			printf("\n\r----------------------------------\n\r");
+			x1 += delta;
+			F = (pow(x1, 3) / -25 + 10 * pow(x1, 2) - 15000);
+			////////////////////////////zakonchilos` mesto////////////////////////////////
+			if (i % 12 == 0)
+			{
+				printf("Press any key\n\r");
+				getch();
+			}
+			////////////////////////////zakonchilos` mesto////////////////////////////////
+			i++;
+		}
+	}
+	}
+
